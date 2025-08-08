48c16fe35a67725a6491f73766ad4adef7355f8b
Ihor Smolianskyi <ihor.smolianskyi@rewconsultingservices.com>
2025-08-08 21:56:03 +0300

    test fix

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  if (!refs.openModalBtn || !refs.closeModalBtn || !refs.modal) return;
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+    document.body.classList.toggle('modal-open');
+  }
+})();
